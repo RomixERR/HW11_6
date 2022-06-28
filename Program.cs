bb6d55acb31007a6d69ee7d0c814de7154dcb2d6
RomixERR <romixerr@mail.ru>
2022-06-28 23:02:01 +0300

    Немного навёл лоска

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace HW11_6
         static FakeUser fakeUser = new FakeUser();
         static void Main(string[] args)
         {
+            firstMenu.HeaderColor = ConsoleColor.Green;
+            firstMenu.Header = "ВАС ПРИВЕТСТВУЕТ БАНК А";
             firstMenu.SubTitle = "Выберете пользователя";
             firstMenu.addMenuItem(0, "Консультант", MItemKonsult);
             firstMenu.addMenuItem(1, "Менеджер", MItemManager);
@@ -28,22 +30,24 @@ namespace HW11_6
         public static void MainMenu()
         {
             Console.Clear();
+            mainMenu.HeaderColor = ConsoleColor.Green;
+            mainMenu.Header = "ВАС ПРИВЕТСТВУЕТ БАНК А";
             while (true)
             {
                 mainMenu.clear();
-                mainMenu.SubTitle = $"==== Меню =================\n" +
+                mainMenu.SubTitle = $"=========М=Е=Н=Ю===========\n" +
                                     $"Вы зашли под [{employer}]\n" +
                                     $"===========================";
                 if (employer is Manager)
                 {
-                    mainMenu.addMenuItem(0, "вывести данные на экран", MItemPrint);
-                    mainMenu.addMenuItem(1, "добавить новую запись", MItemAddNewRecord);
-                    mainMenu.addMenuItem(2, "изменить Телефон", MItemChangePhoneNumber);
-                    mainMenu.addMenuItem(3, "изменить Номер паспорта", MItemChangePasportNumber);
-                    mainMenu.addMenuItem(4, "изменить Фамилию", MItemChangeLastName);
-                    mainMenu.addMenuItem(5, "изменить Имя", MItemChangeFirstName);
-                    mainMenu.addMenuItem(6, "изменить Отчество", MItemChangeMiddleName);
-                    mainMenu.addMenuItem(7, "изменить жене", MItemChangeWife);
+                    mainMenu.addMenuItem(0, "Вывести данные на экран", MItemPrint);
+                    mainMenu.addMenuItem(1, "Добавить новую запись", MItemAddNewRecord);
+                    mainMenu.addMenuItem(2, "Изменить телефон", MItemChangePhoneNumber);
+                    mainMenu.addMenuItem(3, "Изменить номер паспорта", MItemChangePasportNumber);
+                    mainMenu.addMenuItem(4, "Изменить фамилию", MItemChangeLastName);
+                    mainMenu.addMenuItem(5, "Изменить имя", MItemChangeFirstName);
+                    mainMenu.addMenuItem(6, "Изменить отчество", MItemChangeMiddleName);
+                    mainMenu.addMenuItem(7, "Изменить жене", MItemChangeWife);
                 }
                 else
                 {
